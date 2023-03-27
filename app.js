36961f417306eb9861183df71ab31b4b2ca12ce1
nader1414518 <49884739+nader1414518@users.noreply.github.com>
2023-03-27 21:44:45 +0200

    Added Cors Config

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const searoute = require('searoute-js');
+const cors = require("cors");
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+
+app.use(cors({
+  origin: "*",//Hardcoded string of your coming request path or regex for check in string,
+  //Like this: "http://frontend.mydomain.com" or /frontend.mydomain.com/
+  credentials: true //Allow the server to get cookies and other auth data from other 
+  // domains and ports
+}));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false, }));
