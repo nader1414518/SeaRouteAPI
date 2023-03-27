e2c44504dcb68f8c002e295937a048150242bd72
nader1414518 <49884739+nader1414518@users.noreply.github.com>
2023-03-27 21:52:55 +0200

    Added Cors Config Fix

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//   next();
+// });
 
 app.use(cors({
   origin: "*",//Hardcoded string of your coming request path or regex for check in string,
